108104584e8fea0b34299899bd7d8e7ca5021c93
Julik Tarkhanov <me@julik.nl>
2017-07-31 23:05:22 +0200

    Improve Clue styling
    Show a faint dot for "no match"

--- src/game/Clue.js
+++ src/game/Clue.js
@@ -6,17 +6,23 @@ import './Clue.css'
 class Clue extends PureComponent {
   render() {
     const {numExact, numApprox} = this.props
+    const numMissed = 4 - (numExact + numApprox)
+
     const exactDots = Array(Number(numExact)).fill(null).map((_, i) => {
       return <div key={i} className="Dot Exactly" />
     })
     const looselyDots = Array(Number(numApprox)).fill(null).map((_, i) => {
       return <div key={i} className="Dot Loosely" />
     })
+    const missedDots =  Array(Number(numMissed)).fill(null).map((_, i) => {
+      return <div key={i} className="Dot Missed" />
+    })
     
     return(
       <div className="Clue">
         {exactDots }
         {looselyDots }
+        {missedDots }
       </div>
     )
   }
