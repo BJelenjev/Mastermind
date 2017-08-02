4d98b3f75722cfebc5ae8ed8a6fe551d8d006c22
Julik Tarkhanov <me@julik.nl>
2017-08-02 14:03:39 +0200

    Fix syntax in subscribe

--- src/action-creators/subscribe.js
+++ src/action-creators/subscribe.js
@@ -1,5 +1,5 @@
 // src/actions/games/subscribe.js
-import API from '../../api'
+import API from '../api/client'
 
 export const SUBSCRIBED_TO_GAMES_SERVICE = 'SUBSCRIBED_TO_GAMES_SERVICE'
 export const GAME_CREATED_PUSH = 'GAME_CREATED_PUSH'
@@ -14,10 +14,10 @@ const updateGamesList = (gamesArray) => true
  
 export default () => {
   return (dispatch) => {
-    games.on('created', (game) => dispatch({type: GAME_CREATED_PUSH, payload: game))
-    games.on('updated', (game) => dispatch({type: GAME_UPDATED_PUSH, payload: game))
-    games.on('patched', (game) => dispatch({type: GAME_UPDATED_PUSH, payload: game))
-    games.on('removed', (game) => dispatch({type: GAME_REMOVED_PUSH, payload: game))
+    games.on('created', (game) => dispatch({type: GAME_CREATED_PUSH, payload: game}))
+    games.on('updated', (game) => dispatch({type: GAME_UPDATED_PUSH, payload: game}))
+    games.on('patched', (game) => dispatch({type: GAME_UPDATED_PUSH, payload: game}))
+    games.on('removed', (game) => dispatch({type: GAME_REMOVED_PUSH, payload: game}))
     dispatch({ type: SUBSCRIBED_TO_GAMES_SERVICE })
   }
 }
