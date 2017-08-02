29d77c87045f74662025df0109c4e688857427b3
Julik Tarkhanov <me@julik.nl>
2017-08-02 12:03:48 +0200

    Add the create game action creator
    to create actions that create games by calling
    create on the client so that a game is created on
    the server.

---
+++ src/action-creators/create-game.js
@@ -0,0 +1,24 @@
+import APIClient from '../api/client'
+
+const GAME_CREATION_REQUESTED = 'GAME_CREATION_REQUESTED'
+const GAME_CREATED = 'GAME_CREATED'
+
+const enableLoadingState = function(){}
+const disableLoadingState = function(){}
+
+export default (newUserProperties) => {
+  return (dispatch) => {
+    dispatch({type: GAME_CREATION_REQUESTED, payload: {}})
+    enableLoadingState()
+    const client = new APIClient()
+    
+    client.games().create({}).then((result) => {
+      dispatch({type: GAME_CREATED,         payload: result})
+      disableLoadingState()
+    }).catch((error) => {
+      dispatch({type: GAME_CREATION_REJECTED, payload: error})
+      disableLoadingState()
+    })
+  }
+}
+
