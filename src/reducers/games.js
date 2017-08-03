82d90a694a542075b633c083629d834853e51df0
Julik Tarkhanov <me@julik.nl>
2017-08-03 14:33:47 +0200

    Implement correct upsert for the games list
    Add the newly added games to the list, and update
    existing games in the list - BOTH on push AND on
    imperative create

--- src/reducers/games.js
+++ src/reducers/games.js
@@ -3,24 +3,30 @@ const GAME_CREATED_PUSH = 'GAME_CREATED_PUSH'
 const GAME_UPDATED_PUSH = 'GAME_UPDATED_PUSH'
 const GAMES_FETCHED = 'GAMES_FETCHED'
 
+function upsert(games, game) {
+  const ids = games.map((stored) => stored._id)
+  if(ids.includes(game._id)) {   // Update
+    return games.map((storedGame) => {
+      if(storedGame._id === game._id) {
+        return game
+      } else {
+        return storedGame
+      }
+    })
+  } else { // Append
+    return [].concat(games).concat([game])
+  }
+}
 export default (state = [], { type, payload } = {}) => {
   switch (type) {
     case GAMES_FETCHED :
-      return [].concat(payload) // Array of Game-shaped objects
+      return [].concat(payload) // Replace the current state wholesale
     case GAME_CREATED :
-      // do nothing, since the game that has been created just now is going
-      // to be sent to us in a push update
-      return state
+      return upsert(state, payload)
     case GAME_CREATED_PUSH :
-      return state.concat(payload)
+      return upsert(state, payload)
     case GAME_UPDATED_PUSH :
-      return state.map((localGame) => {
-        if(localGame._id === payload._id) {
-          return payload // from remote
-        } else {
-          return localGame
-        }
-      })
+      return upsert(state, payload)
     default :
       return state
   }
