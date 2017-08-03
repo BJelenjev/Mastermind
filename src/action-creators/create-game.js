ec04b239ffcbf6defacff63261389c30b41c90bd
Julik Tarkhanov <me@julik.nl>
2017-08-03 12:57:01 +0200

    Redirect back to lobby after game creation

--- src/action-creators/create-game.js
+++ src/action-creators/create-game.js
@@ -1,5 +1,6 @@
 import APIClient, { FEATHERS_AUTH_TOKEN_KEY } from '../api/client'
 import history from '../history'
+
 const GAME_CREATION_REQUESTED = 'GAME_CREATION_REQUESTED'
 const GAME_CREATION_REJECTED = 'GAME_CREATION_REJECTED'
 const GAME_CREATED = 'GAME_CREATED'
@@ -7,6 +8,7 @@ const GAME_JOINED = 'GAME_JOINED'
 
 const enableLoadingState = function(){}
 const disableLoadingState = function(){}
+
 
 export default (newUserProperties) => {
   return (dispatch) => {
@@ -18,9 +20,7 @@ export default (newUserProperties) => {
       .then(() => {
         client.games().create({}).then((result) => {
           dispatch({type: GAME_CREATED, payload: result})
-          dispatch({type: GAME_JOINED,  payload: result})
-          // redirect via history to the game page
-          history.push(`/game/${result._id}`)
+          history.push('/')
           disableLoadingState()
         }).catch((error) => {
           dispatch({type: GAME_CREATION_REJECTED, payload: error})
